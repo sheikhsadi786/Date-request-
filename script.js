8d115153ed3a862d044ef3938a0a24945ce33536
sheikhsadi786 <sheikhsade786@gmail.com>
2026-07-02 12:12:21 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,40 @@
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyLGBwp72VDwDUR_mWxSs1hTb2LfLJgi8wtm9IurUE8KZnuB4ctNIiVfU0-l0mL7A/exec";
+
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
 const success = document.getElementById("success");
 
-// Make the "No" button run away
 function moveNoButton() {
     const maxX = window.innerWidth - noBtn.offsetWidth - 20;
     const maxY = window.innerHeight - noBtn.offsetHeight - 20;
 
-    const randomX = Math.random() * maxX;
-    const randomY = Math.random() * maxY;
-
-    noBtn.style.left = randomX + "px";
-    noBtn.style.top = randomY + "px";
+    noBtn.style.left = Math.random() * maxX + "px";
+    noBtn.style.top = Math.random() * maxY + "px";
 }
 
 noBtn.addEventListener("mouseover", moveNoButton);
-noBtn.addEventListener("touchstart", function(e){
-    e.preventDefault();
-    moveNoButton();
-});
+noBtn.addEventListener("touchstart", moveNoButton);
 
-// When "Yes" is clicked
-yesBtn.addEventListener("click", () => {
+yesBtn.addEventListener("click", async () => {
 
-    // Confetti
-    confetti({
-        particleCount: 180,
-        spread: 120,
-        origin: { y: 0.6 }
+    const date = document.getElementById("date").value;
+    const time = document.getElementById("time").value;
+    const place = document.getElementById("place").value;
+
+    await fetch(SCRIPT_URL,{
+        method:"POST",
+        body:JSON.stringify({
+            date,
+            time,
+            place
+        })
     });
 
-    // Show success screen
-    success.style.display = "flex";
+    confetti({
+        particleCount:200,
+        spread:120
+    });
 
-    // More confetti
-    setTimeout(() => {
-        confetti({
-            particleCount: 250,
-            spread: 180
-        });
-    }, 700);
+    success.style.display="flex";
 
 });
